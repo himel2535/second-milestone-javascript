4eecd188a35ba941b4e6988f19f55d719369536a
himel2535 <monwarhossanhimel@gmail.com>
2025-08-06 16:27:36 +0600

    acssending and decssending sort

---
+++ sort.js
@@ -0,0 +1,14 @@
+const numbers=[5,34,6,22,10,100,6];
+const sorted_numbers=numbers.sort(function(a,b){return a-b});//assending;
+console.log(sorted_numbers)
+
+const x=[5,34,6,11,22,10,100,68];
+const sorted_x=x.sort(function(a,b){return b-a})//dessending order;
+console.log(x);
+console.log(sorted_x);
+
+// const friends=['akib','xakib','xon','aaron'];
+// sorted_friends=friends.sort();
+// console.log(sorted_friends);
+
+
