03949f8b9c20f1270c16f3c17ca50ce62b4abaf5
himel2535 <monwarhossanhimel@gmail.com>
2025-08-06 23:49:12 +0600

    string sentence to reverse by word with split reverse join

---
+++ array-looping-task4.js
@@ -0,0 +1,12 @@
+// const statement = 'I am a hard working person';
+// let split_statement=statement.split(" ");
+// split_statement.reverse();
+// result=split_statement.join(" ")
+// console.log(result)
+
+// ----another way---->
+
+const statement = 'I am a hard working person';
+// result=statement.split(" ").reverse().join(" ")
+// console.log(result)
+console.log(statement.split(" ").reverse().join(" "))
