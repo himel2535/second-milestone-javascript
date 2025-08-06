58a7a46dbd03e79994058421852d51ef159face4
himel2535 <monwarhossanhimel@gmail.com>
2025-08-06 17:34:15 +0600

    array of object

---
+++ array-of-object.js
@@ -0,0 +1,14 @@
+const students = [
+    {name:'monwar',roll:2120,year:'final year',getJob:false},
+    {name:'anwar',roll:2118,year:'final year',getJob:false},
+    {name:'jafar',roll:2130,year:'final year',getJob:false},
+    {name:'muhim',roll:2121,year:'final year',getJob:false},
+    {name:'murad',roll:398,year:'final year',getJob:true}
+]
+// console.log(students[0])
+
+students[0].name='Himel';
+for (const student of students){
+    console.log(student.name,':',student.roll)
+}
+console.log(students)
