8459a75cd97a3e57faaa30367354d72cef63a695
himel2535 <monwarhossanhimel@gmail.com>
2025-08-06 23:30:12 +0600

    various ways to concat array to string

--- array-looping-task3.js
+++ array-looping-task3.js
@@ -5,3 +5,23 @@ for (let number of numbers){
 }
 console.log(newNumbers)
 
+// ----another way----
+
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// var result = "";
+
+// for (let name of numbers) {
+//   result = result.concat(name);
+// }
+
+// console.log(result); 
+
+// -----another way----->
+
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// var result = numbers.join("");
+
+// console.log(result);
+
+
+
