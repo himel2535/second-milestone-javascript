a4733f6c64aa8b14f2ebf43540199cae169cca54
himel2535 <monwarhossanhimel@gmail.com>
2025-08-06 17:46:39 +0600

    array of array

---
+++ array-of-array.js
@@ -0,0 +1,14 @@
+const numbers=[
+    [23,236,86,99],
+    [2,7,6,36,76],
+    [22,54,12,87,99]
+]
+// console.log(numbers[0][0])
+numbers[0][0]=100;
+// console.log(numbers);
+numbers[0].pop();
+numbers[1].push(200)
+console.log(numbers)
+for (number of numbers){
+    console.log(number);
+}
