48777d5aa3c5bc524997ea0491b84323ecf5d0e7
himel2535 <monwarhossanhimel@gmail.com>
2025-08-06 22:44:28 +0600

    reverse an array task

---
+++ array-looping-task1.js
@@ -0,0 +1,9 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// colors.reverse();
+console.log(colors);
+reversed_colors=[];
+for(color of colors){
+    reversed_colors.unshift(color)
+}
+console.log(reversed_colors)
+
--- array-of-object.js
+++ array-of-object.js
@@ -5,8 +5,7 @@ const students = [
     {name:'muhim',roll:2121,year:'final year',getJob:false},
     {name:'murad',roll:398,year:'final year',getJob:true}
 ]
-// console.log(students[0])
-
+console.log(students[0])
 students[0].name='Himel';
 for (const student of students){
     console.log(student.name,':',student.roll)
