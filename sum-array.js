4a28d104ddf845ebc5e969a4e20b8e629f791c39
himel2535 <monwarhossanhimel@gmail.com>
2025-08-08 00:25:51 +0600

    sum of array using function

---
+++ sum-array.js
@@ -0,0 +1,13 @@
+function sumArray(numbers){
+    console.log(numbers);
+    let sum=0;
+    for(number of numbers){
+        sum=sum+number;
+    }
+    return sum;
+}
+const arr=[2,4,7,3];
+result=sumArray(arr);
+console.log(result)
+// console.log(sumArray([2,4,3,1]))
+
