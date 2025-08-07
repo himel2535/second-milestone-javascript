75c1dc89f3a319f71e2fce6b2c116b01665ac560
himel2535 <monwarhossanhimel@gmail.com>
2025-08-07 23:37:43 +0600

    function with number and string

---
+++ various-params.js
@@ -0,0 +1,33 @@
+function evenSizedString(text){
+    if(text.length %2 === 0){
+        // console.log("wow this is even");
+        return true;
+    }
+    return false;
+    // else{
+    //     // console.log("no this is not even")
+    //     return false;
+    // }
+}
+
+const result1=evenSizedString("mojada")
+const result2=evenSizedString("mojadar")
+const result3=evenSizedString("mojadarkhabar")
+const result4=evenSizedString("kocormocor")
+// console.log(result1,result2,result3,result4);
+
+// -----function with boolean---->
+
+function doubleOrTriple(number,boolean){
+    if (boolean===true){
+        return number*2;
+    }
+    return number*3;
+}
+const x=doubleOrTriple(5,true)
+const y=doubleOrTriple(5,false)
+// console.log(x,y)
+
+
+// ------function with array--->
+
