efbd1465210d71bd988681a15a86d5ec6961441f
himel2535 <monwarhossanhimel@gmail.com>
2025-08-10 01:46:21 +0600

    learning using Math by default function and randomly pick anyone from array

---
+++ math-abs-round-ceil-floor-random.js
@@ -0,0 +1,27 @@
+console.log(Math.PI.toFixed(4))
+
+console.log(Math.abs(-8))
+console.log(Math.abs(66))
+
+console.log(Math.round(9.9))
+console.log(Math.round(9.5))
+
+console.log(Math.ceil(5.1))
+console.log(Math.floor(5.9))
+
+console.log(Math.random())
+
+let rand=Math.random()*10;
+console.log(Math.round(rand))
+
+const minimum=Math.min(1,5,3,7,9);
+console.log(minimum);
+const maximum=Math.max(2,5,4,8,100)
+console.log(maximum)
+
+
+
+const arr = ['moja', 'saja', 'goja', 'aja'];
+const randIndex=Math.floor(Math.random()*arr.length);
+console.log(arr[randIndex])
+
