9678cf1b499ba1deb7249c0e6cf454099aabe16d
himel2535 <monwarhossanhimel@gmail.com>
2025-08-09 22:49:06 +0600

    inch to feet prblem solved by function

---
+++ get-distance-problem2.js
@@ -0,0 +1,3 @@
+const km=100;
+mile=km*0.621371;
+console.log(mile.toFixed(2), 'Miles')
--- get-feet-problem1.js
+++ get-feet-problem1.js
@@ -1,6 +1,14 @@
-const height=66;
-feetHeight=height/12;
-// console.log(feetHeight)
-// console.log(parseInt(feetHeight))
-inchHeight=height%12;
-console.log(parseInt(feetHeight),"feet", inchHeight,"inch")
+// const height=66;
+// feetHeight=height/12;
+// // console.log(feetHeight)
+// // console.log(parseInt(feetHeight))
+// inchHeight=height%12;
+// console.log(parseInt(feetHeight),"feet", inchHeight,"inch")
+
+function inchToFeet(inch){
+    // const inch=75;
+    feetHeight=inch/12;
+    inchHeight=inch%12;
+    console.log(parseInt(feetHeight),'feet',inchHeight,'inch')
+}
+inchToFeet(66);
