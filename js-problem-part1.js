fbcb2bbf057134c4b2ad63506a4f93aadec50bb7
himel2535 <monwarhossanhimel@gmail.com>
2025-08-10 17:44:25 +0600

    celsius to farhenheit

--- js-problem-part1.js
+++ js-problem-part1.js
@@ -1,7 +1,16 @@
-function f2c(f){
-    c=(f-32)*5/9;
-    return c;
-}
-result=f2c(98);
-console.log(result.toFixed(2),'Degree')
+// function f2c(f){
+//     c=(f-32)*5/9;
+//     return c;
+// }
+// result=f2c(98);
+// console.log(result.toFixed(2),'Degree')
 
+
+function c2f(c){
+    f=(c*9/5)+32;
+    return f;
+}
+// const c=40;
+result=c2f(40);
+console.log(result,'degree fahrenheit')
+// console.log(`${c} degree celsius = ${result} degree fahrenheit`);
