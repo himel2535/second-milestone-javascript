8a0a1ac71e97ea4f5c83b8b9698efeaea0d5ebbe
himel2535 <monwarhossanhimel@gmail.com>
2025-08-13 18:20:03 +0600

    getting random number between 10 to 20

---
+++ random-number.js
@@ -0,0 +1,10 @@
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(getRandomInt(5, 15));
+
+function random_int(min,max){
+    return Math.floor(Math.random()*(max - min +1)+min)
+}
+console.log(random_int(10,20))
