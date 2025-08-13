c869ead9bd6af4babce7de028392ddc807300cb3
himel2535 <monwarhossanhimel@gmail.com>
2025-08-13 17:55:13 +0600

    finding longest word in string

---
+++ longest-word.js
@@ -0,0 +1,19 @@
+let sentence="I am learning Programming to become a programmer"
+let text=sentence.toLowerCase()
+let words=text.split(" ");
+function longestWord(words){
+    let long=[0];
+    for (word of words){
+        if(word.length>long.length){
+            long=word;
+        }
+    }
+    return long;
+}
+const result=longestWord(words);
+console.log(result);
+
+
+
+// word=sentence.split(" ")
+// console.log(word)
